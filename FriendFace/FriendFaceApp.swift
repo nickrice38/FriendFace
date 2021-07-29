bc26e598f40bdd80db43f71c97883c23fc22a9f0
Nick Rice <enprice383@gmail.com>
2021-07-29 17:43:00 +0100

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Nick Rice on 29/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
